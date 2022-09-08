799be26f5325c4177617721e79ed15cf62c57931
Nagathi <ragbr7070@gmail.com>
2022-09-08 20:04:56 -0300

    Commit

--- Exe 17/src/Main.java
+++ Exe 17/src/Main.java
@@ -1,4 +1,69 @@
+import java.util.*;
+import javax.swing.JOptionPane;
+import java.io.*;
 
 public class Main {
-
+	public static void main(String [] args) throws IOException, ClassNotFoundException{
+		
+		Map <Integer, Object> pessoas = new HashMap();
+		
+		FileInputStream read_clientes = new FileInputStream("pessoas.dat");
+		ObjectInputStream in = new ObjectInputStream(read_clientes);
+		pessoas = (HashMap) in.readObject();
+		
+		String option = JOptionPane.showInputDialog(
+													"1 - Cadastrar 			\n" +
+													"2 - Consultar Pessoa 	\n" +
+													"3 - Imprimir Arquivos 	\n" +
+													"F - Encerrar Programa\n\n" );
+														
+		while(!option.equals("F")){
+			if(option.equals("1")) {
+				int id 		= Integer.parseInt(JOptionPane.showInputDialog("ID"));
+				String nome = 				  (JOptionPane.showInputDialog("Nome"));
+				String tel  = 				  (JOptionPane.showInputDialog("Telefone"));
+				
+				Pessoa pessoa = new Pessoa(nome, tel);
+				
+				pessoas.put(id, pessoa);
+				JOptionPane.showMessageDialog(null, "Cadastrado!");
+			}
+			
+			if(option.equals("2")) {
+				Integer search = Integer.parseInt(JOptionPane.showInputDialog("ID: "));
+				
+				Iterator it = pessoas.entrySet().iterator();
+				
+				while(it.hasNext()) {
+					HashMap.Entry mp = (HashMap.Entry) it.next();
+					
+					for (Map.Entry<Integer, Object> set : pessoas.entrySet()) {
+					    if(search.equals(set.getKey())) {
+					    	JOptionPane.showMessageDialog(null, "Encontrado!");
+					    	Pessoa p = (Pessoa) set.getValue();
+					    	JOptionPane.showMessageDialog(null, "ID: " 			+ search + "\n" + 
+																"Nome: " 		+ p.getNome() + "\n" +
+																"Telefone: " 	+ p.getTel());
+					    }
+					}
+				}
+			}
+			
+			if(option.equals("3")) {
+				
+				FileOutputStream write_pessoas = new FileOutputStream("pessoas.dat");
+				ObjectOutputStream out = new ObjectOutputStream(write_pessoas);
+				out.writeObject(pessoas);
+				
+				JOptionPane.showMessageDialog(null, "Dados salvos!");
+			}
+			
+			option = JOptionPane.showInputDialog(
+												 "1 - Cadastrar 		 \n" +
+												 "2 - Consultar Pessoa 	 \n" +
+												 "3 - Salvar Informações \n" +
+												 "F - Encerrar Programa\n\n" );
+		}
+	}
+	
 }
--- Exe 17/src/Pessoa.java
+++ Exe 17/src/Pessoa.java
@@ -1,4 +1,27 @@
+import java.io.Serializable;
 
-public class Pessoa {
+public class Pessoa implements Serializable {
+	private String nome;
+	private String tel;
+	
+	public Pessoa(String nome, String tel){
+		this.setNome(nome);
+		this.setTel(tel);
+	}
 
+	public String getTel() {
+		return tel;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
 }
